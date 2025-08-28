4de591888e9325796d8c4428485c3c28a679078d
Arthur <arthursilvaa2614@gmail.com>
2025-08-28 19:58:35 -0300

    Exercicio funções

---
+++ função para verificar par ou impar.py
@@ -0,0 +1,8 @@
+def verificar_par_ou_impar(numero):
+    if numero % 2 == 0:
+        return "par"
+    else:
+        return "ímpar"
+
+print(verificar_par_ou_impar(7)) 
+print(verificar_par_ou_impar(10)) 
---
+++ função que calcula o fatorial.py
@@ -0,0 +1,10 @@
+def fatorial(n):
+    if n < 0:
+        raise ValueError("O número deve ser não negativo.")
+    resultado = 1
+    for i in range(2, n + 1):
+        resultado *= i
+    return resultado
+
+numero = int(input("Digite um número para calcular o fatorial: "))
+print(f"O fatorial de {numero} é {fatorial(numero)}")
